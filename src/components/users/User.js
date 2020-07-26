9277c955947e598a8c1cf4ccde75ff17e3f7949d
Chuluq <choirulchuluq10@gmail.com>
2020-07-26 14:45:16 +0700

    implementing context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -30,7 +34,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 
   return (
     <Fragment>
-      <Link to='/' classname='btn btn-light'>
+      <Link to='/' className='btn btn-light'>
         Back to Search{' '}
       </Link>
       Hireable:{' '}
@@ -93,20 +97,12 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
       <div className='card text-center'>
         <div className='badge badge-primary'>Followers: {followers}</div>
         <div className='badge badge-success'>Following: {following}</div>
-        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-danger'>Public Repos: {public_repos}</div>
         <div className='badge badge-dark'>Public Gists: {public_gists}</div>
       </div>
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.array.isRequired,
 };
 
 export default User;
